6c45f8a83090c5cb1fd42c21706bf7bb5098cdad
Yaswanth M <yaswanth2k3@gmail.com>
2025-07-06 16:17:07 +0530

    Add files via upload

---
+++ 1st-jango/mysite/hello/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def home(request):
+    temp=loader.get_template('index.html')
+    return HttpResponse(temp.render())
+
